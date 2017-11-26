34ee31987816fdf35adb9365437bcd0670aa6e71
haozheng <hza89@sfu.ca>
2017-11-25 16:49:30 -0800

    Initial commit

---
+++ MainActivity.java
@@ -0,0 +1,88 @@
+package com.bignerdranch.android.databasetest;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MyDatabaseHelper dbHelper;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        dbHelper = new MyDatabaseHelper(this, "BookStore.db", null, 2);
+        Button createDatabase = (Button) findViewById(R.id.create_database);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dbHelper.getWritableDatabase();
+            }
+        });
+
+        //add new data  CRUD OPERATIONS
+        Button addData = (Button) findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("name", "The wtf");
+                values.put("author", "hao");
+                values.put("pages", 450);
+                values.put("price", 18);
+                db.insert("Book", null, values);
+                values.clear();
+            }
+        });
+
+        //update data
+        Button updataData = (Button) findViewById(R.id.update_data);
+        updataData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("price", 11);
+                db.update("Book", values, "name = ?", new String[] {"The da vince code"});
+            }
+        });
+
+        //delete data
+        Button deleteButton = (Button) findViewById(R.id.delete_data);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                db.delete("Book", "pages > ?", new String[] {"500"});
+            }
+        });
+
+        //query data
+        Button queryButton = (Button) findViewById(R.id.query_data);
+        queryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                Cursor cursor = db.query("Book", null, null, null, null, null, null);
+                if (cursor.moveToFirst()) {
+                    do {
+                        //遍历cursor对象， 取出数据并打印
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+
+                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                    } while(cursor.moveToNext());
+                }
+                cursor.close();
+            }
+        });
+    }
+}
---
+++ MyDatabaseHelper.java
@@ -0,0 +1,52 @@
+package com.bignerdranch.android.databasetest;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+/**
+ * Created by zhenghao on 2017-11-20.
+ */
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+    public static final String CREATE_BOOK = "create table Book ("
+            + "id integer primary key autoincrement, "
+            + "author text, "
+            + "price real, "
+            + "pages integer, "
+            + "name text)";
+
+    public static final String CREATE_CATEGORY = "create table Category ("
+            + "id integer primary key autoincrement, "
+            + "category_name text, "
+            + "category_code integer)";
+
+
+    private Context mContext;
+
+    public MyDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory
+                            factory, int version) {
+        super(context, name, factory, version);
+        mContext = context;
+    }
+
+    //onCreate() gets called when we call getReadableDatabase() or getWritableDatabase()
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_BOOK);
+        Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists Book");
+        db.execSQL("Drop table if exists Category");
+        onCreate(db);
+
+    }
+
+
+
+}
